08201841b3ded288d657b006e5926522b6a5967f
Peter Milne <peter.milne@d-tacq.com>
2016-02-26 11:41:06 +0000

    PACKAGE: create 70-mdsshell package for distribution 	new file:   PACKAGE/CARE/example-profile-mdsshell.sh 	new file:   PACKAGE/CARE/mdsputch-postshot-example 	new file:   PACKAGE/bin/get.xdt 	new file:   PACKAGE/init/mdsshell.init 	new file:   PACKAGE/make.package 	new file:   PACKAGE/opkg/root/.mdsPutCh.nosh 	new file:   PACKAGE/opkg/root/.mdsPutCh.sh 	new file:   PACKAGE/opkg/root/README 	new file:   PACKAGE/opkg/usr/local/CARE/example-profile-mdsshell.sh 	new file:   PACKAGE/opkg/usr/local/CARE/mdsputch-postshot-example 	new file:   PACKAGE/opkg/usr/local/bin/get.xdt 	new file:   PACKAGE/opkg/usr/local/bin/mdsPutCal 	new file:   PACKAGE/opkg/usr/local/bin/mdsPutCh 	new file:   PACKAGE/opkg/usr/local/bin/mdsshell 	new file:   PACKAGE/opkg/usr/local/bin/mdsshell.inet 	new file:   PACKAGE/opkg/usr/local/bin/readVolts 	new file:   PACKAGE/opkg/usr/local/init/mdsshell.init

---
+++ PACKAGE/CARE/example-profile-mdsshell.sh
@@ -0,0 +1,3 @@
+# copy to /etc/profile.d/mdsshell.sh to activate
+
+MDSSHELL_ARGS="-v 1"
---
+++ PACKAGE/opkg/root/.mdsPutCh.sh
@@ -0,0 +1,6 @@
+# .mdsPutCh.sh - variables with shell interpretation
+# HostName is used for canonical card name convention
+echo %HN=`hostname`
+echo %XDT=`get.xdt`
+
+
