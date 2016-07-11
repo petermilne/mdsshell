eb02a80d957b48ed8bb6473c99dde0fe681b1685
Peter Milne <peter.milne@d-tacq.com>
2016-07-11 20:09:57 +0100

    acq400_fs_ioctl.h needed for Start Samples Stride control 	new file:   acq400_fs_ioctl.h

---
+++ mdsshell/acq400_fs_ioctl.h
@@ -0,0 +1,33 @@
+/* ------------------------------------------------------------------------- */
+/* acq400_fs_ioctl.h  D-TACQ ACQ400 FMC  DRIVER                                   
+ * Project: ACQ420_FMC
+ * Created: 26 Mar 2016  			/ User: pgm
+ * ------------------------------------------------------------------------- *
+ *   Copyright (C) 2016 Peter Milne, D-TACQ Solutions Ltd         *
+ *                      <peter dot milne at D hyphen TACQ dot com>           *
+ *                                                                           *
+ *  This program is free software; you can redistribute it and/or modify     *
+ *  it under the terms of Version 2 of the GNU General Public License        *
+ *  as published by the Free Software Foundation;                            *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU General Public License for more details.                             *
+ *                                                                           *
+ *  You should have received a copy of the GNU General Public License        *
+ *  along with this program; if not, write to the Free Software              *
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                *
+ *
+ * TODO 
+ * TODO
+ * ------------------------------------------------------------------------- */
+
+#ifndef ACQ400_FS_IOCTL_H_
+#define ACQ400_FS_IOCTL_H_
+
+#define ACQ400_FS_STRIDE	0xa400f001
+
+
+
+#endif /* ACQ400_FS_IOCTL_H_ */
